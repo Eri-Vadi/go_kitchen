0209527ac243223b8e7659b14c1e9734b342ad7f
Eric <90966216+Eri-Vadi@users.noreply.github.com>
2021-10-10 21:08:24 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"context"
+	"github.com/Eri-Vadi/go_kitchen/application"
+	"github.com/Eri-Vadi/go_kitchen/internal/infrastracture/config"
+	"github.com/Eri-Vadi/go_kitchen/internal/infrastracture/profiler"
 	"os"
 	"os/signal"
 	"syscall"
-
-	"github.com/Eri-Vadi/go_kitchen/application"
-	"github.com/Eri-Vadi/go_kitchen/internal/infrastracture/config"
 )
 
-func init() {
+func init(){
 	config.LoadConfig()
 }
 
-func main() {
-	ctx, cancel := context.WithCancel(context.Background())
+func main(){
+	profiler.StartProfiler()
+
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
 
@@ -23,7 +23,4 @@ func main() {
 	go app.Start()
 
 	<-sigChan
-
-	app.Shutdown(ctx)
-	cancel()
 }
