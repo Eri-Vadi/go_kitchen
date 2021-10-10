0e1a820a2977367e0c2f1f0420b84714dfc8b733
Eric <90966216+Eri-Vadi@users.noreply.github.com>
2021-10-10 21:03:24 +0300

    Update cook.go

--- internal/service/cook/cook.go
+++ internal/service/cook/cook.go
@@ -1,10 +1,10 @@
 package cook
 
 import (
+	"github.com/Eri-Vadi/go_kitchen/internal/domain/entity"
+	"github.com/Eri-Vadi/go_kitchen/internal/infrastracture/logger"
 	"sync"
 	"time"
-
-	"github.com/Eri-Vadi/go_kitchen/internal/domain/entity"
 )
 
 type State int
@@ -16,16 +16,25 @@ const (
 
 type Cook struct {
 	statusMutex sync.Mutex
-	status      State
+	status State
 
-	id          int
-	rank        int
-	proficiency int
-	name        string
-	phrase      string
+	idMutex sync.Mutex
+	id int
+
+	currentItemsMutex sync.Mutex
+	currentItems int
+
+	entity.Cook
 }
 
-func (c *Cook) GetState() State {
+func (c *Cook) GetID() int {
+	c.idMutex.Lock()
+	defer c.idMutex.Unlock()
+
+	return c.id
+}
+
+func (c *Cook) GetState() State{
 	c.statusMutex.Lock()
 	defer c.statusMutex.Unlock()
 	return c.status
@@ -37,27 +46,43 @@ func (c *Cook) SetState(newState State) {
 	c.statusMutex.Unlock()
 }
 
-func (c *Cook) Prepare(food entity.Food, foodID int, idChannel chan<- int) {
-	c.SetState(Busy)
+func (c *Cook) Prepare(food entity.Food,foodID int, idChannel chan<- int) {
+	c.itemsDecr()
 	time.Sleep(time.Second * time.Duration(food.PreparationTime))
 
-	idChannel <- foodID
-	c.SetState(Free)
+	idChannel<-foodID	
+	c.itemsIncr()
 }
 
-func CookEntityToService(cookEntities []entity.Cook) []Cook {
+func EntityToService(cookEntities []entity.Cook) []Cook {
 	var response []Cook
 
 	for idx, val := range cookEntities {
 		response = append(response, Cook{
 			status:      Free,
 			id:          idx,
-			rank:        val.Rank,
-			proficiency: val.Proficiency,
-			name:        val.Name,
-			phrase:      val.CatchPhrase,
+			Cook: val,
+			currentItems: val.Proficiency,
 		})
 	}
 
 	return response
+}
+
+func (c *Cook) itemsIncr(){
+	c.currentItemsMutex.Lock()
+	defer c.currentItemsMutex.Unlock()
+	c.SetState(Free)
+	c.currentItems++
+}
+
+func (c *Cook) itemsDecr(){
+	c.currentItemsMutex.Lock()
+	defer c.currentItemsMutex.Unlock()
+	if c.currentItems < 1 {
+		logger.LogErrorF("Trying to decrement already zero items!")
+	}else if c.currentItems == 1 {
+		c.SetState(Busy)
+	}
+	c.currentItems--
 }
