d8f63f9192a2b240535bfc78da60caa0c24bcad9
Eric <90966216+Eri-Vadi@users.noreply.github.com>
2021-10-10 20:48:40 +0300

    Update kitchen.go

--- application/kitchen.go
+++ application/kitchen.go
@@ -2,12 +2,11 @@ package application
 
 import (
 	"context"
-	"net/http"
-
 	"github.com/Eri-Vadi/go_kitchen/internal/http/controller"
 	"github.com/Eri-Vadi/go_kitchen/internal/infrastracture/logger"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
+	"net/http"
 )
 
 type IApp interface {
@@ -23,35 +22,30 @@ func CreateApp() IApp {
 	appHandler := gin.New()
 
 	ctrl := controller.NewKitchenController()
-
 	ctrl.RegisterKitchenRoutes(appHandler)
 
 	app := kitchenApp{
 		server: &http.Server{
-			Addr:    viper.GetString("kitchen_host"),
-			Handler: appHandler,
+			Addr:              viper.GetString("kitchen_host"),
+			Handler:           appHandler,
 		},
 	}
+	ctrl.Initialize()
 
 	return &app
 }
 
-func (d *kitchenApp) initialize() {
-
-}
-
 func (d *kitchenApp) Start() {
-	d.initialize()
 	logger.LogMessage("Starting kitchen server")
 
 	if err := d.server.ListenAndServe(); err != http.ErrServerClosed {
-		logger.LogPanicF("Unexpected error while running server: %v", err)
+		logger.LogPanicf("Unexpected error while running server: %v", err)
 	}
 }
 
 func (d *kitchenApp) Shutdown(ctx context.Context) {
 	if err := d.server.Shutdown(ctx); err != nil {
-		logger.LogPanicF("Unexpected error while closing server: %v", err)
+		logger.LogPanicf("Unexpected error while closing server: %v", err)
 	}
 	logger.LogMessage("Server terminated successfully")
 }
